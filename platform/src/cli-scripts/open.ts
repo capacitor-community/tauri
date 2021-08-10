91f825d565d7b53eb3c9a545f5a84247f1ea91e7
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2021-08-10 10:54:40 -0400

    chore: fmt

--- platform/src/cli-scripts/open.ts
+++ platform/src/cli-scripts/open.ts
@@ -7,7 +7,9 @@ export async function doOpen(): Promise<void> {
   const usersProjectDir = process.env.CAPACITOR_ROOT_DIR;
   const destDir = join(usersProjectDir, 'tauri');
   try {
-    await runExec(`cd ${destDir} && npm run build && cd src-tauri/target/debug && ${configData.appName}.exe`);
+    await runExec(
+      `cd ${destDir} && npm run build && cd src-tauri/target/debug && ${configData.appName}.exe`,
+    );
   } catch (e) {
     errorLog(e.message);
     throw e;
